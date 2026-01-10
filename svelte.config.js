78d0cb84078488718f46b42ec46e9cfed7f40d79
Abhishek Thulasi <abhishek45nine@gmail.com>
2026-01-10 21:42:13 +0530

    Setup Cloudflare Configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown.
 			pages: 'build', // Output directory for pages (index.html will go here)
 			assets: 'build', // Output directory for assets
 			fallback: '404.html', // Prevents overwriting the actual index.html
